1e1105025b64d49c7729c01c70cfa6e20a2adb12
yangqizhang <yqz0203@hotmail.com>
2023-07-19 22:26:52 +0800

    fix: 禁用express etag

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { Express } from 'express';
 import { AppModule } from './app.module';
 import { ValidateBody } from './common/decorators/validate.decorator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 禁用express的etag
+  (app.getHttpAdapter() as unknown as Express).set('etag', false);
 
   app.enableCors();
 
