2c0ba8969fd10afac2cfff5cb54da1b969d5af68
yangqizhang <yqz0203@hotmail.com>
2023-07-19 17:52:17 +0800

    feat: cdn支持

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
+const env = loadEnv('', '');
+
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   server: { port: 3333, host: '0.0.0.0' },
+  base: env.VITE_BASE_URL,
   plugins: [
     react(),
     svgr({
       svgrOptions: { svgProps: { fill: 'currentColor' } },
     }),
   ],
-});
+}));
---
+++ packages/uploadcos/index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+require('dotenv/config');
+
+console.log(process.env.COS_KEY);
+
+// 引入模块
+const COS = require('cos-nodejs-sdk-v5');
+const glob = require('glob');
+const chunk = require('lodash.chunk');
+// 创建实例
+const cos = new COS({
+  SecretId: process.env.COS_KEY,
+  SecretKey: process.env.COS_SECRET,
+});
+
+const Bucket = process.env.COS_BUCKET;
+const Region = process.env.COS_REGION;
+
+const appName = process.argv[2] || 'test';
+
+async function uploadFiles(files) {
+  await Promise.all(
+    files.map(item => {
+      console.log('> 上传：', item.filePath);
+
+      return cos.sliceUploadFile({
+        Bucket: Bucket,
+        Region: Region,
+        Key: item.key,
+        FilePath: item.filePath, // 本地文件地址，需自行替换
+      });
+    }),
+  );
+}
+
+async function start() {
+  let files = glob.globSync('./dist/**/*');
+
+  files = files.map(item => ({
+    key: item.replace('dist', appName),
+    filePath: item,
+  }));
+
+  const chunked = chunk(files, 5);
+  let index = 0;
+
+  while (chunked[index]) {
+    await uploadFiles(chunked[index]);
+    index++;
+  }
+
+  console.log('> Upload Cos done.');
+}
+
+start();
